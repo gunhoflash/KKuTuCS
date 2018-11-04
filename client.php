b2c7276c8760956ade1a5213bf8e7859bb35cec2
Geonho Kim <gunhoflash@gmail.com>
2018-11-05 00:40:09 +0900

    Test.

--- client.php
+++ client.php
@@ -1,31 +1,38 @@
 <?php
 $str = $_POST['str'];
-$client = stream_socket_client("tcp://127.0.0.1:7000", $errno, $errorMessage);
+$client = stream_socket_client("https://kkutucs.herokuapp.com/7000", $errno, $errorMessage);
 if ($client === false)
 {
-	throw new UnexpectedValueException("Failed to connect: $errno - $errorMessage");
+	echo "Failed to connect: $errno - $errorMessage";
+	fclose($client);
+	return;
+	//throw new UnexpectedValueException("Failed to connect: $errno - $errorMessage");
+}
+else
+{
+	/*
+	* generate a message
+	*/
+	$message = "KKuTuCS".PHP_EOL.$str;
+	fwrite($client, $message);
+
+	/*
+	* response
+	*/
+	while (true) {
+		$response = stream_get_contents($client, 1);
+		echo $response;
+		$info = stream_get_meta_data($client);
+
+		if ($info['eof'] || $info['unread_bytes'] === 0)
+			break;
+	}
+
+	/*
+	* bye bye
+	*/
+	fclose($client);
 }
 
-/*
- * generate a message
- */
-$message = "KKuTuCS".PHP_EOL.$str;
-fwrite($client, $message);
 
-/*
- * response
- */
-while (true) {
-	$response = stream_get_contents($client, 1);
-	echo $response;
-	$info = stream_get_meta_data($client);
-
-	if ($info['eof'] || $info['unread_bytes'] === 0)
-		break;
-}
-
-/*
- * bye bye
- */
-fclose($client);
 ?>
--- server.php
+++ server.php
@@ -3,7 +3,7 @@ include './libs/time.php';
 include './libs/Request.php';
 defined('PROJECT_ROOT') or define('PROJECT_ROOT', __DIR__ . '/');
 set_time_limit(0); 
-$server = stream_socket_server("tcp://127.0.0.1:7000", $errno, $errorMessage);
+$server = stream_socket_server("https://kkutucs.herokuapp.com/7000", $errno, $errorMessage);
 
 if ($server === false)
 {
